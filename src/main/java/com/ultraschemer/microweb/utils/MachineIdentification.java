7a0294fc8d5a59fef378febbf17625e34020209c
Paulo Silva Filho <paulosfilho@gmail.com>
2018-09-12 00:12:33 -0300

    Backporting project back to Java 1.8.

--- src/main/java/com/ultraschemer/microweb/utils/MachineIdentification.java
+++ src/main/java/com/ultraschemer/microweb/utils/MachineIdentification.java
@@ -34,7 +34,19 @@ public class MachineIdentification {
      * @return The process id.
      */
     public static long getProcessId() {
-        return ProcessHandle.current().pid();
+        // TODO: Release the final implementation, presented below, after Java 10 is stable enough.
+
+        // The next code is compatible to OpenJDK Java 1.8, and Oracle Java 1.8 and it's a workaround
+        // that can be used safely on Windows and Linux. Other operating systems aren't supported.
+        String processName =
+                java.lang.management.ManagementFactory.getRuntimeMXBean().getName();
+        return Long.parseLong(processName.split("@")[0]);
+
+        // The next code is suitable to Java 9 and after.
+        // To backport the project to Java 8, the workaround presented above
+        // has been used. When Java 10 is stable enough, then this final
+        // implementation can be used.
+        // return ProcessHandle.current().pid();
     }
 
     /**
