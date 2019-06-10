d89ea1b26343726ed685e4ccf20f7387db1dc79a
Paulo Silva Filho <paulosfilho@gmail.com>
2019-06-09 23:21:22 -0300

    Adding the SimpleController base class, to improve the development of controllers.

---
+++ src/main/java/com/ultraschemer/microweb/vertx/SimpleController.java
@@ -0,0 +1,64 @@
+package com.ultraschemer.microweb.vertx;
+
+import com.google.common.base.Throwables;
+import com.ultraschemer.microweb.domain.bean.Message;
+import com.ultraschemer.microweb.error.StandardException;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.core.json.Json;
+import io.vertx.ext.web.Route;
+import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.handler.BodyHandler;
+
+public abstract class SimpleController implements BasicController {
+    private String contentType;
+    private int httpStatusCode;
+    private String errorCode;
+
+    public SimpleController (int errorHttpStatusCode, String errorCode) {
+        contentType = "application/json; charset=utf-8";
+        this.httpStatusCode = errorHttpStatusCode;
+        this.errorCode = errorCode;
+    }
+
+    public SimpleController(String contentType, int errorHttpStatusCode, String errorCode) {
+        this.contentType = contentType;
+        this.httpStatusCode = errorHttpStatusCode;
+        this.errorCode = errorCode;
+    }
+
+    @Override
+    public void evaluate(Route route) {
+        route.handler(BodyHandler.create()).blockingHandler(routingContext -> {
+            HttpServerResponse response = routingContext.response();
+            response.putHeader("Content-Type", contentType);
+
+            try {
+                executeEvaluation(routingContext, response);
+            } catch(StandardException se) {
+                // Ensure the correct content-type in the case of error:
+                response.putHeader("Content-Type", contentType);
+                // Finish the response:
+                response.setStatusCode(se.getHttpStatus()).end(Json.encode(se.bean()));
+            } catch(Throwable t) {
+                Message msg = new Message(this.errorCode, this.httpStatusCode,
+                        "Unknown error: " + t.getMessage(),
+                        Throwables.getStackTraceAsString(t));
+                // Ensure the correct content-type in the case of error:
+                response.putHeader("Content-Type", contentType);
+                // Finish the response:
+                response.setStatusCode(msg.getHttpStatus()).end(Json.encode(msg));
+            }
+        });
+    }
+
+    public abstract void executeEvaluation(RoutingContext routingContext, HttpServerResponse response)
+            throws StandardException;
+
+    protected String getContentType() {
+        return contentType;
+    }
+
+    protected void setContentType(String contentType) {
+        this.contentType = contentType;
+    }
+}
