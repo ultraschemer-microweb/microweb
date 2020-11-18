0d0430f74f5374703c1c6ddf8424f4a827ce5536
Ultraschemer <paulosfilho@gmail.com>
2020-11-18 01:05:29 -0300

    Reducing the timeout to communicate with KeyCloak to a minimum - since keycloak slow communication turns the entire Microweb unstable.

--- src/main/java/com/ultraschemer/microweb/domain/CentralUserRepositoryManagement.java
+++ src/main/java/com/ultraschemer/microweb/domain/CentralUserRepositoryManagement.java
@@ -28,9 +28,9 @@ import java.util.stream.Collectors;
 
 public class CentralUserRepositoryManagement {
     private static final OkHttpClient client = new OkHttpClient.Builder()
-            .connectTimeout(45, TimeUnit.SECONDS)
-            .writeTimeout(45, TimeUnit.SECONDS)
-            .readTimeout(45, TimeUnit.SECONDS)
+            .connectTimeout(5, TimeUnit.SECONDS)
+            .writeTimeout(5, TimeUnit.SECONDS)
+            .readTimeout(5, TimeUnit.SECONDS)
             .build();
     private static JsonObject wellKnown = null;
     private static JsonObject masterWellKnown = null;
